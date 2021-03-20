933849571f297f5d4d2512da8bc9d5b17bd16eec
evgen chichilov <jetset.3566@gmail.ocm>
2021-03-20 20:42:47 +0600

    Add weapon assets, add inventoryComponent, Add HealthComponent, add intractableItems, add StatsWidgets, Finished PickUp Logic

--- DemoMultiPlayer/Source/DemoMultiPlayer/DemoMultiPlayerGameMode.cpp
+++ DemoMultiPlayer/Source/DemoMultiPlayer/DemoMultiPlayerGameMode.cpp
@@ -9,9 +9,9 @@ ADemoMultiPlayerGameMode::ADemoMultiPlayerGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	//DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
-	HUDClass = ADemoMultiPlayerHUD::StaticClass();
+	//HUDClass = ADemoMultiPlayerHUD::StaticClass();
 }
